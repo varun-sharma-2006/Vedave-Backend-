bc6d14f3ee38fda4771839c03d628dbbbb05a770
Varun Sharma <varunsharma42006@gmail.com>
2026-07-26 18:13:55 +0530

    feat(auth): implement JWT login authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'vedave-super-secret-key',
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+  ],
+
   controllers: [AuthController],
+
   providers: [AuthService],
+
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,20 @@
+import { JwtService } from '@nestjs/jwt';
 import {
   ConflictException,
   Injectable,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+  private prisma: PrismaService,
+  private jwtService: JwtService,
+) {}
 
   async register(registerDto: RegisterDto) {
     const { firstName, lastName, email, phone, password } = registerDto;
@@ -45,7 +51,7 @@ export class AuthService {
       },
     });
 
-        return {
+    return {
       success: true,
       message: 'User registered successfully',
       data: {
@@ -57,5 +63,47 @@ export class AuthService {
         createdAt: user.createdAt,
       },
     };
+  }
+
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const passwordMatched = await bcrypt.compare(
+      password,
+      user.passwordHash ?? '',
+    );
+
+    if (!passwordMatched) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const payload = {
+  sub: user.id,
+  email: user.email,
+};
+
+const accessToken = await this.jwtService.signAsync(payload);
+
+return {
+  success: true,
+  message: 'Login successful',
+  accessToken,
+  data: {
+    id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    phone: user.phone,
+    createdAt: user.createdAt,
+  },
+};
   }
 }
